6829d7c4d1e54a67aa3b97af1daf63fb63a041ea
Lyvia GG <lyvia.guerre-genton@cgi.com>
2025-08-25 10:28:48 +0200

    (/) change screen orientation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,8 +7,19 @@ import { Provider } from 'react-redux';
 // import store from '@/redux/store';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { store } from '@/redux/store/store';
+import * as ScreenOrientation from 'expo-screen-orientation';
+import { useEffect } from 'react';
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    const unlockScreenOerientation = async () => {
+      await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+    };
+    unlockScreenOerientation();
+  }, []);
+
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
