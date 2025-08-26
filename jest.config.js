afaae0b9b5f03b5a9a1e3f30e44234257eef6e3e
Lyvia GG <lyvia.guerre-genton@cgi.com>
2025-08-26 11:23:28 +0200

    (+) init test unitiaire jest

---
+++ jest.config.js
@@ -0,0 +1,43 @@
+// module.exports = {
+//   preset: 'jest-expo',
+//   transform: {
+//     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+//   },
+//   moduleNameMapper: {
+//     '^@/(.*)$': '<rootDir>/$1',
+//   },
+//   transformIgnorePatterns: [
+//     'node_modules/(?!(react-native|expo|@expo|@react-native|@testing-library)/)',
+//   ],
+// };
+
+// /** @type {import('jest').Config} */
+// module.exports = {
+//   preset: 'jest-expo',
+//   transform: {
+//     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+//   },
+//   moduleNameMapper: {
+//     '^@/(.*)$': '<rootDir>/$1',
+//   },
+//   transformIgnorePatterns: [
+//     // 'node_modules/(?!(expo|expo-router|expo-modules-core|react-native|@expo|@react-native|@testing-library)/)',
+//     'node_modules/(?!(expo-router|expo-linking|expo-modules-core|@react-navigation|react-native|react-native-.*|@react-native|@expo)/)',
+//   ],
+//   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect', '<rootDir>/setup-jest.js'],
+// };
+
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'jest-expo',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(expo|expo-router|expo-linking|expo-modules-core|@react-navigation|react-native|react-native-.*|@react-native|@expo)/)',
+  ],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect', '<rootDir>/setup-jest.js'],
+};
---
+++ setup-jest.js
@@ -0,0 +1,22 @@
+// const matchers = require('jest-extended');
+// expect.extend(matchers);
+
+// afterEach(() => {
+//   jest.useRealTimers();
+// });
+
+import '@testing-library/jest-native/extend-expect';
+
+// Mock des modules Expo/React Native qui posent souvent problème
+jest.mock('expo-router', () => ({
+  useRouter: () => ({ push: jest.fn(), replace: jest.fn(), back: jest.fn() }),
+}));
+
+jest.mock('expo-secure-store', () => ({
+  getItemAsync: jest.fn(),
+  setItemAsync: jest.fn(),
+  deleteItemAsync: jest.fn(),
+}));
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+);
