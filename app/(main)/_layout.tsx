7e5a2c90c63b56db943fd0180a109d55717793e6
Lyvia GG <lyvia.guerre-genton@cgi.com>
2025-08-27 10:46:43 +0200

    (/) app/_layout : stack navigation

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -1,17 +1,9 @@
-import { Tabs } from 'expo-router';
+import { Stack } from 'expo-router';
 import React, { useEffect } from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Platform, View } from 'react-native';
 import * as ScreenOrientation from 'expo-screen-orientation';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   // useEffect(() => {
   //   const unlockScreenOerientation = async () => {
   //     await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
@@ -20,35 +12,48 @@ export default function TabLayout() {
   // }, []);
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+    // <Tabs
+    //   screenOptions={{
+    //     tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+    //     headerShown: false,
+    //     tabBarButton: HapticTab,
+    //     tabBarBackground: TabBarBackground,
+    //     tabBarStyle: Platform.select({
+    //       ios: {
+    //         // Use a transparent background on iOS to show the blur effect
+    //         position: 'absolute',
+    //       },
+    //       default: {},
+    //     }),
+    //   }}
+    // >
+    //   <Tabs.Screen
+    //     name="index"
+    //     options={{
+    //       title: 'Home',
+    //       tabBarIcon: ({}) => <IconSymbol size={28} name="house.fill" color={'black'} />,
+    //     }}
+    //   />
+    //   <Tabs.Screen
+    //     name="explore"
+    //     options={{
+    //       title: 'Explore',
+    //       tabBarIcon: ({}) => <IconSymbol size={28} name="paperplane.fill" color={'black'} />,
+    //     }}
+    //   />
+    // </Tabs>
+
+    <View style={{ flex: 1 }}>
+      <Stack
+        screenOptions={{
+          // Hide the header for all other routes.
+          headerShown: false,
         }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
+      >
+        {/* <Stack.Screen name="(main)" options={{ headerShown: false }} /> 
+        <Stack.Screen name="+not-found" />  */}
+      </Stack>
+      {/* <StatusBar style="auto" /> */}
+    </View>
   );
 }
