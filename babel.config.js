3e67b6d5a1446901b57c0e9bfec0ff23c8804bbf
Lyvia GG <lyvia.guerre-genton@cgi.com>
2025-08-28 09:28:35 +0200

    (+) .env variable

--- babel.config.js
+++ babel.config.js
@@ -7,6 +7,21 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo', '@babel/preset-typescript'],
+    presets: [
+      'babel-preset-expo',
+      // '@babel/preset-typescript',
+      // 'module:metro-react-native-babel-preset',
+    ],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+    ],
   };
 };
