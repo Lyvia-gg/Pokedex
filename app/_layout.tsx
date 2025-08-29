4582593cee65d91ffa06abd69fe75a7edde0afa2
Lyvia GG <lyvia.guerre-genton@cgi.com>
2025-08-29 08:03:35 +0200

    (/) layout stack animation fade

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -41,7 +41,7 @@ function RootNavigator() {
   // const { session } = useSession();
 
   return (
-    <Stack>
+    <Stack screenOptions={{ animation: 'fade' }}>
       <Stack.Protected guard={!!session}>
         <Stack.Screen name="(main)" options={{ headerShown: false }} />
       </Stack.Protected>
