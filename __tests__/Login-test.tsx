72f705e4dd06d841c646734086f7a1695e7219e3
Lyvia GG <lyvia.guerre-genton@cgi.com>
2025-08-29 11:43:28 +0200

    (/) login-test : provider redux

--- __tests__/Login-test.tsx
+++ __tests__/Login-test.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Login from '@/app/(auth)/index';
 import { render, screen, fireEvent } from '@testing-library/react-native';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import reducer from '@/redux/reducers/pokemonReducer';
 // import { describe, expect, test } from '@jest/globals';
 
 describe('<Login />', () => {
@@ -14,45 +17,38 @@ describe('<Login />', () => {
   // ###################################################### Faire un test pour voir si <Login/> se rend bien
   beforeEach(() => {
     // initialisation avant chaque test (factorisation)
-    render(<Login />);
+    const store = configureStore({ reducer: reducer });
+    render(
+      <Provider store={store}>
+        <Login />
+      </Provider>,
+    );
     submitButton = screen.getByTestId('submitButton');
     expectedEmail = 'chuck.noris@gmail.com';
     expectedPassword = "Au commencement, il regarda Arceus crée l'univers";
   });
 
   test('Login : champs vide & bouton désactivé', async () => {
-    try {
-      expect(submitButton).toBeDisabled();
-    } catch (error) {
-      console.log(error);
-    }
+    fireEvent.changeText(screen.getByTestId('inputEmail'), '');
+    fireEvent.changeText(screen.getByTestId('inputPassword'), '');
+    expect(submitButton).toBeDisabled();
   });
 
   test('Login : email rempli & bouton désactivé', async () => {
-    try {
-      fireEvent.changeText(screen.getByTestId('inputEmail'), expectedEmail);
-      expect(submitButton).toBeDisabled();
-    } catch (error) {
-      console.log(error);
-    }
+    fireEvent.changeText(screen.getByTestId('inputEmail'), expectedEmail);
+    fireEvent.changeText(screen.getByTestId('inputPassword'), '');
+    expect(submitButton).toBeDisabled();
   });
 
   test('Login : password & bouton désactivé', async () => {
-    try {
-      fireEvent.changeText(screen.getByTestId('inputPassword'), expectedPassword);
-      expect(submitButton).toBeDisabled();
-    } catch (error) {
-      console.log(error);
-    }
+    fireEvent.changeText(screen.getByTestId('inputEmail'), '');
+    fireEvent.changeText(screen.getByTestId('inputPassword'), expectedPassword);
+    expect(submitButton).toBeDisabled();
   });
 
   test('Login : champs rempli & bouton activé ', async () => {
-    try {
-      fireEvent.changeText(screen.getByTestId('inputEmail'), expectedEmail);
-      fireEvent.changeText(screen.getByTestId('inputPassword'), expectedPassword);
-      expect(submitButton).not.toBeDisabled();
-    } catch (error) {
-      console.log(error);
-    }
+    fireEvent.changeText(screen.getByTestId('inputEmail'), expectedEmail);
+    fireEvent.changeText(screen.getByTestId('inputPassword'), expectedPassword);
+    expect(submitButton).not.toBeDisabled();
   });
 });
