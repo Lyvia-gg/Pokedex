511b8eaf48c80c2c297adf4133af229afb772e00
Lyvia-GG <lyvia.guerre-genton@cgi.com>
2025-09-01 09:57:30 +0200

    #### Correction bug babel ####

--- babel.config.js
+++ babel.config.js
@@ -7,21 +7,18 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      'babel-preset-expo',
-      // '@babel/preset-typescript',
-      // 'module:metro-react-native-babel-preset',
-    ],
+    presets: ['babel-preset-expo', '@babel/preset-typescript'],
     plugins: [
-      [
-        'module:react-native-dotenv',
-        {
-          moduleName: '@env',
-          path: '.env',
-          safe: false,
-          allowUndefined: true,
-        },
-      ],
+      // [
+      //   'module:react-native-dotenv',
+      //   {
+      //     moduleName: '@env',
+      //     path: '.env',
+      //     safe: false,
+      //     allowUndefined: true,
+      //   },
+      // ],
+      'react-native-reanimated/plugin',
     ],
   };
 };
--- constants/api.ts
+++ constants/api.ts
@@ -1,26 +1,32 @@
-import { API_URL } from '@env';
-
+// import { EXPO_PUBLIC_API_URL } from '@env';
+// const EXPO_PUBLIC_API_URL = 'https://pokeapi.co/api/v2';
 export const getPokedexOffsetApi = async (offset: number): Promise<Response> => {
   // return fetch('https://pokeapi.co/api/v2/' + genName);
   if (offset) {
-    return fetch(API_URL + '/pokemon?limit=100&offset=' + offset);
+    return fetch(process.env.EXPO_PUBLIC_API_URL + '/pokemon?limit=100&offset=' + offset);
   }
-  return fetch(API_URL + '/pokemon?limit=100&offset=0');
+  return fetch(process.env.EXPO_PUBLIC_API_URL + '/pokemon?limit=100&offset=0');
 };
 
 export const getPokemonApi = async (pokedex_id: number): Promise<Response> => {
-  return fetch(API_URL + '/pokemon/' + pokedex_id);
+  return fetch(process.env.EXPO_PUBLIC_API_URL + '/pokemon/' + pokedex_id);
 };
 export const getDetailsPokemonApi = async (pokedex_id: number): Promise<Response> => {
-  return fetch(API_URL + '/pokemon-species/' + pokedex_id);
+  return fetch(process.env.EXPO_PUBLIC_API_URL + '/pokemon-species/' + pokedex_id);
 };
 
-export const getPokemonByFormApi = async (form: string): Promise<Response> => {
+export const getPokemonByFormApi = async (form?: string): Promise<Response> => {
   //result.pokemon_species -> tout les pokemons aillant cette forme
-  return fetch(API_URL + '/pokemon-shape/' + form);
+  if (form) {
+    return fetch(process.env.EXPO_PUBLIC_API_URL + '/pokemon-shape/' + form);
+  }
+  return fetch(process.env.EXPO_PUBLIC_API_URL + '/pokemon-shape/');
 };
 
-export const getPokemonByTypeApi = async (type: string): Promise<Response> => {
+export const getPokemonByTypeApi = async (type?: string): Promise<Response> => {
   //result.pokemon -> tout les pokemons aillant ce type
-  return fetch(API_URL + '/type/' + type);
+  if (type) {
+    return fetch(process.env.EXPO_PUBLIC_API_URL + '/type/' + type);
+  }
+  return fetch(process.env.EXPO_PUBLIC_API_URL + '/type/');
 };
