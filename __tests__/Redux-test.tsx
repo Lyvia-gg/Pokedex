7aed7a5f5b488715f1ddde5fa79a9da8606ac319
Lyvia-GG <lyvia.guerre-genton@cgi.com>
2025-09-05 11:08:14 +0200

    (/) test Redux : fix bug

--- __tests__/Redux-test.tsx
+++ __tests__/Redux-test.tsx
@@ -8,6 +8,7 @@ import {
   signIn,
   signOut,
 } from '@/redux/actions/pokemonAction';
+import { IFilters } from '@/redux/store/type';
 describe('Redux', () => {
   let store: any;
   let expectedEmail: string;
@@ -16,24 +17,28 @@ describe('Redux', () => {
     store = configureStore({ reducer: reducer });
     expectedEmail = 'chuck.noris@gmail.com';
   });
+
   test('Get Pokedex', async () => {
     await store.dispatch(getPokedex());
     const state = store.getState().pokemons.pokemonList;
 
     expect(state.length).toBeGreaterThan(0);
   });
+
   test('signIn', async () => {
     await store.dispatch(signIn(expectedEmail));
     const state = store.getState().user;
 
     expect(state.email).toBe(expectedEmail);
   });
+
   test('signOut', async () => {
     await store.dispatch(signOut());
     const state = store.getState().user;
 
     expect(state).toBeNull();
   });
+
   test('selectPokemon', async () => {
     await store.dispatch(selectPokemon(1));
     const state = store.getState().pokemons.selectedPokemon;
@@ -47,27 +52,35 @@ describe('Redux', () => {
 
     expect(state).not.toBeNull();
   });
+
   test('setFilter : type normal, form null', async () => {
-    await store.dispatch(setFilter({ form: null, type: 'normal' }));
-    const state = store.getState().filters.filter;
+    const filter: IFilters = { form: null, type: 'normal', searchingValue: null };
+    await store.dispatch(setFilter(filter));
+    const state = store.getState().pokemons.filters;
 
     expect(state).not.toBeNull();
   });
+
   test('setFilter : type normal, form quadriped', async () => {
-    await store.dispatch(setFilter({ form: 'quadriped', type: 'normal' }));
-    const state = store.getState().filters.filter;
+    const filter: IFilters = { form: 'quadriped', type: 'normal', searchingValue: null };
+    await store.dispatch(setFilter(filter));
+    const state = store.getState().pokemons.filters;
 
     expect(state).not.toBeNull();
   });
+
   test('setFilter : type null, form quadriped', async () => {
-    await store.dispatch(setFilter({ form: 'quadriped', type: null }));
-    const state = store.getState().filters.filter;
+    const filter: IFilters = { form: 'quadriped', type: null, searchingValue: null };
+    await store.dispatch(setFilter(filter));
+    const state = store.getState().pokemons.filters;
 
     expect(state).not.toBeNull();
   });
+
   test('setFilter : type null, form null', async () => {
-    await store.dispatch(setFilter({ form: null, type: null }));
-    const state = store.getState().filters.filter;
+    const filter: IFilters = { form: null, type: null, searchingValue: null };
+    await store.dispatch(setFilter(filter));
+    const state = store.getState().pokemons.filters;
 
     expect(state).toBeNull();
   });
